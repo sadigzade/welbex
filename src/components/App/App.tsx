fc18ef06e4db635f0508b48b4cb7c43873d58f47
Khikmet Sadigzade <sadigzade.hi@mail.ru>
2023-07-22 22:31:33 +0300

    Implemented the background and header for the page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+import purpleBallSvg from "../../assets/images/purple-ball.svg";
+import redBallSvg from "../../assets/images/red-ball.svg";
+
 import styles from "./App.module.css";
 
 const App = () => {
-  return <div />;
+  return (
+    <div className={styles.app}>
+      <div className={styles.app__wrapper}>
+        <Header />
+        <main className={styles.app__main}></main>
+        <Footer />
+      </div>
+      <div className={styles.app__balls}>
+        <div className={styles["app__balls-purple"]}>
+          <img src={purpleBallSvg} alt="Purple Ball" />
+        </div>
+        <div className={styles["app__balls-red--big"]}>
+          <img src={redBallSvg} alt="Red Ball" />
+        </div>
+        <div className={styles["app__balls-red--small"]}>
+          <img src={redBallSvg} alt="Red Ball" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
